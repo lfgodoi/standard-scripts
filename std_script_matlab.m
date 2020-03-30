11b0a4413b27fc03f2aca1fe05e2ad9bfab4474d
Leonardo Godói <eng.leonardogodoi@gmail.com>
2020-03-30 20:04:03 -0300

    Initial commit

---
+++ std_script_matlab.m
@@ -0,0 +1,21 @@
+%%
+
+% TITLE: 
+   % Standard script structure for MATLAB programming language
+
+% DESCRIPTION:
+   % Standard structure for scripts written using MATLAB 
+   % programming language.
+   
+% VERSION: 
+   % Author: Leonardo Godói (eng.leonardogodoi@gmail.com)
+   % Creation date: 16-February-2018
+
+% REVISION HISTORY:
+   % V1.0 | 16-February-2018 | Leonardo Godói | Creation
+
+%% --------------------------------------------------------------
+
+% Code here
+
+%% --------------------------------------------------------------
